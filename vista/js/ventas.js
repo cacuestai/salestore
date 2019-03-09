23cb39e8915301476264975d3e725e0e729f1377
cacuestai <cacuestai@yahoo.es>
2019-03-09 17:49:54 -0500

    Versión 0.06 - Probando el formulario de ventas...

--- vista/js/ventas.js
+++ vista/js/ventas.js
@@ -16,9 +16,8 @@ new class Venta {
         this.url = './controlador/fachada.php'; // la url del controlador de fachada
         this.filasPorPagina = 7;
 
-        this.inicializarClientes();
-
         $('#venta-vendedor').value = 'El vendedor autenticado';
+        this.inicializarClientes();
     }
 
     /**
@@ -66,6 +65,7 @@ new class Venta {
         });
     }
 
+
     /**
      * Intenta cargar la lista de productos que es posible seleccionar para la venta, si sucede 
      * algún error en esta parte, no se continúa configurando el formulario de ventas, si hay
@@ -79,19 +79,21 @@ new class Venta {
             }
         }).then(productos => {
             if (productos.ok) {
-                this.crearLineasDeVenta(productos);
+                this.crearLineasDeVenta(productos, this.calcularLineaVenta, this.calcularTotal);
             } else {
                 throw new Error(productos.mensaje);
             }
         });
     }
 
+
+
     /**
      * Configura una tabla en donde es posible agregar o eliminar líneas o detalles de venta.
      * @param {Object} productos Un objeto que contiene dos listas de productos, una con todos los 
      * datos de cada producto y otra con los datos básicos para visualizar en un campo de autocompletar.
      */
-    crearLineasDeVenta(productos) {
+    crearLineasDeVenta(productos, _calcularLineaVenta, _calcularTotal) {
         this.tablaVentas = new Tabulator("#tabla-ventas", {
             height: "200px",
             movableColumns: true,
@@ -106,7 +108,11 @@ new class Venta {
                         min: 1,
                         max: 1000
                     },
-                    align: "right"
+                    align: "right",
+                    cellEdited: function(celda) {
+                        _calcularLineaVenta(celda, productos);
+                        _calcularTotal(celda.getRow().getTable().getData());
+                    }
                 },
                 {
                     title: "Descripción de productos",
@@ -116,30 +122,68 @@ new class Venta {
                         values: productos.lista_minima,
                     },
                     cellEdited: function(celda) {
-                        this.filaActual = celda.getRow().getData();
-                        let idProducto = this.filaActual.producto.split('-')[0];
-                        let producto = productos.lista_completa.find(obj => obj.id_producto == idProducto);
-                        this.filaActual.valor = producto.precio;
-                        this.filaActual.iva_porcentaje = producto.porcentaje_iva;
-                        this.filaActual.iva_valor = producto.porcentaje_iva * producto.porcentaje_iva * this.filaActual.cantidad;
-                        this.filaActual.subtotal = this.filaActual.cantidad * producto.precio + this.filaActual.iva_valor;
-                        celda.getRow().update(this.filaActual);
+                        _calcularLineaVenta(celda, productos);
+                        _calcularTotal(celda.getRow().getTable().getData());
                     }
                 },
                 { title: "Vr. Unitario", field: "valor", width: 100, align: "right" },
                 { title: "% IVA", field: "iva_porcentaje", width: 100, align: "right" },
                 { title: "Vr. IVA", field: "iva_valor", width: 100, align: "right" },
-                { title: "Subtotale", field: "subtotal", width: 100, align: "right" }
+                { title: "Subtotale", field: "subtotal", width: 100, align: "right" },
+                { // la última columna incluye un botón para eliminar líneas de ventas
+                    title: 'Control',
+                    headerSort: false,
+                    width: 65,
+                    align: "center",
+                    formatter: (cell, formatterParams) => {
+                        // en cada fila, en la última columna, se asignan botones para eliminar las líneas de venta
+                        return '<i id="tabulator-btneliminar" class="material-icons deep-orange-text">delete</i>';
+                    },
+                    cellClick: (e, celda) => {
+                        if (celda) {
+                            celda.getRow().delete();
+                            _calcularLineaVenta(celda, productos);
+                            _calcularTotal(celda.getRow().getTable().getData());
+                        }
+                    }
+                }
             ]
         });
 
         let btnAgregar = $('#venta-btnagregar');
         btnAgregar.addEventListener('click', event => {
+            // OJO si, l fila anterior no ha sido completada, NO permitir agregar  **************
             this.operacion = 'insertar';
             this.tablaVentas.addRow({ cantidad: 1, producto: '' }, false); // agregar una fila en blanco al final
         });
         btnAgregar.click();
     }
+
+    calcularTotal(data) {
+        let total = 0;
+        data.forEach(function(elemento) {
+            total += elemento.subtotal;
+        });
+        $('#venta-total').value = total;
+        M.updateTextFields();
+    }
+
+    calcularLineaVenta(celda, productos) {
+        let filaActual = celda.getRow().getData();
+        let idProducto = filaActual.producto.split('-')[0];
+        let producto = productos.lista_completa.find(obj => obj.id_producto == idProducto);
+
+        if (producto) {
+            filaActual.valor = producto.precio;
+            filaActual.iva_porcentaje = producto.porcentaje_iva;
+            filaActual.iva_valor = producto.porcentaje_iva * producto.porcentaje_iva * filaActual.cantidad;
+            filaActual.subtotal = filaActual.cantidad * producto.precio + filaActual.iva_valor;
+            celda.getRow().update(filaActual);
+        }
+    }
+
+
+
 
 
 
